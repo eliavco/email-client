645b3361c6d8313701e97ec5636585a785ab9427
Eliav Cohen <eliav.s.cohen@gmail.com>
2020-06-20 03:57:14 +0300

    version update aliases

--- src/app/components/update-alias/update-alias.component.ts
+++ src/app/components/update-alias/update-alias.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { User } from './../../interfaces/user';
+import { UserService } from './../../services/user/user.service';
+import { AlertsService } from './../../services/alerts/alerts.service';
+import { AuthenticationService } from './../../services/authentication/authentication.service';
 
 @Component({
 	selector: 'bk-update-alias',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./update-alias.component.scss']
 })
 export class UpdateAliasComponent implements OnInit {
+	@Input() user: User;
+	alias = '';
+	@Output() refresh = new EventEmitter<void>();
+	err: string;
 
-	constructor() { }
+	constructor(
+		private userService: UserService,
+		private authenticationService: AuthenticationService,
+		private alertsService: AlertsService) { }
 
 	ngOnInit(): void {
+	}
+
+	onSubmit() {
+		this.userService.addSubscription(this.alias).subscribe(() => {
+			this.refresh.emit();
+		}, err => {
+			this.err = err;
+		});
 	}
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -9,19 +9,23 @@ export class UserService {
 	constructor(private http: HttpClient) { }
 
 	getAll() {
-		return this.http.get<User[]>(`${environment.apiUrl}/api/v${environment.apiVersion}/users`);
+		return this.http.get(`${environment.apiUrl}/api/v${environment.apiVersion}/users`);
 	}
 
 	getOne(id: string) {
-		return this.http.get<User[]>(`${environment.apiUrl}/api/v${environment.apiVersion}/users${id}`);
+		return this.http.get(`${environment.apiUrl}/api/v${environment.apiVersion}/users${id}`);
 	}
 
 	updateSelf(data) {
-		return this.http.patch<User[]>(`${environment.apiUrl}/api/v${environment.apiVersion}/users/updateInfo`, data);
+		return this.http.patch(`${environment.apiUrl}/api/v${environment.apiVersion}/users/updateInfo`, data);
 	}
 
 	updatePassword(data) {
-		return this.http.patch<User[]>(`${environment.apiUrl}/api/v${environment.apiVersion}/users/updatePassword`, data);
+		return this.http.patch(`${environment.apiUrl}/api/v${environment.apiVersion}/users/updatePassword`, data);
+	}
+
+	addSubscription(alias: string) {
+		return this.http.patch(`${environment.apiUrl}/api/v${environment.apiVersion}/users/sub?alias=${alias}`, null);
 	}
 
 }
